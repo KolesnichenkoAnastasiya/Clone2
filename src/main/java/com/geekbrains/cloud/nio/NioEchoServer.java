46f0c9179fa801be43c15cc1823e7bb84ff823e0
KolesnichenkoAnastasiya <22028@mail.ru>
2022-01-16 20:26:20 +0300

    Done HW lesson 2 in NioEchoServer2.java

--- src/main/java/com/geekbrains/cloud/nio/NioEchoServer.java
+++ src/main/java/com/geekbrains/cloud/nio/NioEchoServer.java
@@ -3,11 +3,11 @@ package com.geekbrains.cloud.nio;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -25,13 +25,15 @@ public class NioEchoServer {
     private ServerSocketChannel serverChannel;
     private Selector selector;
     private ByteBuffer buf;
+    private Path currentDir;
+    private final String WORKING_DIR_ROOT = "workingDir";
 
     public NioEchoServer() throws IOException {
-        buf = ByteBuffer.allocate(5);
+        buf = ByteBuffer.allocate(50);
         serverChannel = ServerSocketChannel.open();
         selector = Selector.open();
         serverChannel.configureBlocking(false);
-        serverChannel.bind(new InetSocketAddress(8189));
+                serverChannel.bind(new InetSocketAddress(8195));
         serverChannel.register(selector, SelectionKey.OP_ACCEPT);
         System.out.println("Server started...");
         while (serverChannel.isOpen()) {
@@ -71,10 +73,108 @@ public class NioEchoServer {
             }
             buf.clear();
         }
-        // process(s)
+
         System.out.println("Received: " + s);
-        channel.write(ByteBuffer.wrap(s.toString().getBytes(StandardCharsets.UTF_8)));
+        String[] tokens = s.toString().split("\\s+", 2);
+        String command = tokens[0];
+        String param = tokens[1].trim();
+        switch (command) {
+            case "ls":
+                sendFileList(channel);
+                break;
+            case "cd":
+                changeDir(channel, param);
+                break;
+            case "cat":
+                viewFile(channel, param);
+                break;
+            case "mkdir":
+                makeDir(channel, param);
+                break;
+            case "touch":
+                createFile(channel, param);
+                break;
+            default:
+                channel.write(ByteBuffer.wrap(s.toString().getBytes(StandardCharsets.UTF_8)));
+        }
     }
+
+    private void viewFile(SocketChannel channel, String param) throws IOException {
+        try {
+            Path path = currentDir.resolve(param);
+            if (path.toFile().isDirectory()) {
+                channel.write(ByteBuffer.wrap(("Not a file provided " + param + "\n\r").getBytes(StandardCharsets.UTF_8)));
+                return;
+            }
+            ByteChannel file = Files.newByteChannel(path);
+
+            buf.clear();
+            int read = 0;
+            while (true) {
+                read = file.read(buf);
+                if (read == 0){
+                    break;
+                }
+                if (read < 0){
+                    file.close();
+                    break;
+                }
+                buf.flip();
+                channel.write(buf);
+                buf.clear();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.write(ByteBuffer.wrap(("Failed to read file " + param + " [" + e.getClass().getName() + "]" + "\n\r").getBytes(StandardCharsets.UTF_8)));
+        }
+    }
+
+    private void createFile(SocketChannel channel, String name) throws IOException {
+        try {
+            Files.createFile(currentDir.resolve(Paths.get(name)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.write(ByteBuffer.wrap(("Failed to create file " + name + " [" + e.getClass().getName() + "]" + "\n\r").getBytes(StandardCharsets.UTF_8)));
+        }
+    }
+
+    private void makeDir(SocketChannel channel, String name) throws IOException {
+        try {
+            Files.createDirectory(currentDir.resolve(Paths.get(name)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.write(ByteBuffer.wrap(("Failed to create directory " + name + " [" + e.getClass().getName() + "]" + "\n\r").getBytes(StandardCharsets.UTF_8)));
+        }
+    }
+
+    private void changeDir(SocketChannel channel, String param) throws IOException {
+        if (currentDir.getFileName().toString().equals(WORKING_DIR_ROOT) && param.equals("..")) return;
+        try {
+            Path dir = Paths.get(param);
+            if (currentDir.resolve(dir).toFile().exists()) {
+                currentDir = currentDir.resolve(dir);
+                if (currentDir.getFileName().toString().equals("..")) {
+                    currentDir = currentDir.getRoot().resolve(currentDir.subpath(0, currentDir.getNameCount() - 2));
+                }
+                channel.write(ByteBuffer.wrap(("Current working path is /" + currentDir.getFileName().toString() + "\n\r").getBytes(StandardCharsets.UTF_8)));
+                System.out.println(currentDir.toString());
+            } else {
+                channel.write(ByteBuffer.wrap(("Directory " + param + " doesn`t exist" + "\n\r").getBytes(StandardCharsets.UTF_8)));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.write(ByteBuffer.wrap(("Can`t resolve given path. " + param + "\n\r").getBytes(StandardCharsets.UTF_8)));
+        }
+    }
+
+    private void sendFileList(SocketChannel channel) throws IOException {
+        String[] fileArray = currentDir.toFile().list();
+        for (String item : fileArray) {
+            channel.write(ByteBuffer.wrap((item + "\n\r").getBytes(StandardCharsets.UTF_8)));
+        }
+    }
+
+//}
 
     private void handleAccept() throws IOException {
         SocketChannel channel = serverChannel.accept();
@@ -90,3 +190,4 @@ public class NioEchoServer {
         new NioEchoServer();
     }
 }
+
---
+++ src/main/java/com/geekbrains/cloud/nio/NioEchoServer2Starter.java
@@ -0,0 +1,8 @@
+package com.geekbrains.cloud.nio;
+import java.io.IOException;
+
+public class NioEchoServer2Starter {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new NioEchoServer2();
+    }
+}
