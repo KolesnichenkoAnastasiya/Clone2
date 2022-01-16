08bdd239ce3c4115af5e45ba7b7387589f9b55aa
KolesnichenkoAnastasiya <79326207+KolesnichenkoAnastasiya@users.noreply.github.com>
2022-01-16 20:45:28 +0300

    Update NioEchoServer2Starter.java

--- src/main/java/com/geekbrains/cloud/nio/NioEchoServer2Starter.java
+++ src/main/java/com/geekbrains/cloud/nio/NioEchoServer2Starter.java
@@ -2,7 +2,7 @@ package com.geekbrains.cloud.nio;
 import java.io.IOException;
 
 public class NioEchoServer2Starter {
-    public static void main(String[] args) throws IOException, InterruptedException {
+        public static void main(String[] args) throws IOException, InterruptedException {
         new NioEchoServer2();
     }
 }
